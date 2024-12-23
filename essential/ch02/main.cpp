fa03e1a9ab974a63226cab249e26dd4e7ac3084d
Ferris <ferris.evans@outlook.com>
2024-12-24 00:08:29 +0800

    2/5

--- essential/ch02/main.cpp
+++ essential/ch02/main.cpp
@@ -3,8 +3,12 @@
 #include <vector>
 
 void swap(int &, int &);
-void display(std::vector<int>);
+
+void display(const std::vector<int> &, std::ostream & = std::cout);
+
 void bubble_sort(std::vector<int>);
+
+const std::vector<int> *fibon_seq(int);
 
 int main() {
     int ival = 1024; // 对象，类型为int
@@ -40,18 +44,40 @@ void swap(int &a, int &b) {
     a = b;
     b = temp;
 }
-void display(std::vector<int> vec) {
-    for (int i = 0; i < 8; i++) std::cout << vec[i] << " ";
-    std::cout << std::endl;
+
+void display(const std::vector<int> &vec, std::ostream &os) {
+    for (int i = 0; i < 8; i++) os << vec[i] << " ";
+    os << std::endl;
+}
+
+const std::vector<int> *fibon_seq(int size) {
+    // 局部静态对象
+    static std::vector<int> elems;
+    const int max_size = 1024;
+
+    if (size <= 0 || size > max_size) {
+        std::cerr << "Invalid size " << size << std::endl;
+        return 0;
+    }
+
+    // 如果size等于或小于elems.size()就不必重新计算了
+    for (int i = elems.size(); i < size; i++) {
+        if (i == 0 || i == 1) elems.push_back(1);
+        else elems.push_back(elems[i - 1] + elems[i - 2]);
+    }
+
+    return &elems;
 }
 
 // 对文件的读写操作
 std::ofstream ofil("text_out1.txt");
+
 void bubble_sort(std::vector<int> vec) {
     for (int i = 0; i < vec.size(); i++) {
-        for (int j = i+1; j < vec.size(); j++) {
+        for (int j = i + 1; j < vec.size(); j++) {
             if (vec[i] > vec[j]) {
-                ofil << "about to call swap!" << "i: " << i << " j: " << j << '\t' << " swapping: " << vec[i] << " with " << vec[j] << std::endl;
+                ofil << "about to call swap!" << "i: " << i << " j: " << j << '\t' << " swapping: " << vec[i] <<
+                        " with " << vec[j] << std::endl;
                 swap(vec[i], vec[j]);
             }
         }
