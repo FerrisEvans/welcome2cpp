2391ceb94fe99f61a8aa0f0fb01d0de538f97809
Ferris <ferris.evans@outlook.com>
2025-01-17 14:24:11 +0800

    reference

--- cherno/p16PointersInCpp/main.cpp
+++ cherno/p16PointersInCpp/main.cpp
@@ -4,17 +4,41 @@
 
 void allocate();
 
-int main() {
+void ref();
 
+void pointer();
+
+void increase(int &);
+
+int main() {
+    int a = 1;
+    increase(a);
+    LOG(a);
+    return 0;
+}
+
+void pointer() {
     int var = 8;
     int *ptr = &var;
     *ptr = 10;
     LOG(var)
-    return 0;
 }
 
 void allocate() {
-    char* buffer = new char[8];
+    char *buffer = new char[8];
     memset(buffer, 0, 8);
     delete[] buffer;
+}
+
+void ref() {
+    int a = 5;
+    int &ref = a;
+
+    ref = 2;
+    LOG(a)
+}
+
+
+void increase(int &val) {
+    val++;
 }
