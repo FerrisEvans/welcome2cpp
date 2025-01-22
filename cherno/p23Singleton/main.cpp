1abe6a693b77a2517b764532aaffa05e848c87f7
Ferris <ferris.evans@outlook.com>
2025-01-23 00:41:56 +0800

    p23 singleton with static sample

---
+++ cherno/p23Singleton/main.cpp
@@ -0,0 +1,15 @@
+class Singleton {
+
+public:
+    static Singleton &Get() {
+        static Singleton instance;
+        return instance;
+    }
+
+    void Hello() {}
+};
+
+int main() {
+    Singleton::Get().Hello();
+    return 0;
+}
