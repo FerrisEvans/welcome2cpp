50a8a201f2efdb278d3e2798859701a827b6f97d
Ferris <ferris.evans@outlook.com>
2025-01-27 04:04:55 +0800

    p29 pure virtual function / interface

--- cherno/p27Extend/main.cpp
+++ cherno/p27Extend/main.cpp
@@ -10,6 +10,9 @@ public:
     }
 
     virtual std::string GetName() { return "Entity"; }
+
+    // pure virtual function. This will make Entity an abstract class. We can't make an instance for Entity.
+    virtual std::string IGetName() = 0;
 };
 
 class Player : public Entity {
@@ -21,9 +24,12 @@ public:
     }
 
     std::string GetName() override { return m_Name; }
+
+    std::string IGetName() override { return m_Name; }
 };
 
 void PrintName(Entity* e);
+void PrintIName(Entity* e);
 
 int main() {
     Player player;
@@ -31,13 +37,19 @@ int main() {
     std::cout << player.X << " " << player.Y << std::endl;
     std::cout << sizeof(player) << std::endl;
 
-    Entity entity;
-    PrintName(&entity);
+    // Entity entity;
+    // PrintName(&entity);
+    // PrintIName(&entity);
     PrintName(&player);
+    PrintIName(&player);
     return 0;
 }
 
 
 void PrintName(Entity* e) {
     std::cout << e->GetName() << std::endl;
+}
+
+void PrintIName(Entity* e) {
+    std::cout << e->IGetName() << std::endl;
 }
