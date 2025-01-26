5a2b3ba3f66c489ba8164e346ed7fb9536705266
Ferris <ferris.evans@outlook.com>
2025-01-26 21:26:15 +0800

    p28 virtual function

--- cherno/p27Extend/main.cpp
+++ cherno/p27Extend/main.cpp
@@ -8,21 +8,36 @@ public:
         X += xa;
         Y += ya;
     }
+
+    virtual std::string GetName() { return "Entity"; }
 };
 
 class Player : public Entity {
+    std::string m_Name;
 public:
-    char* Name;
     Player() : Entity() {
-        Name = "";
+        m_Name = "Player";
         X = Y = 0;
     }
+
+    std::string GetName() override { return m_Name; }
 };
+
+void PrintName(Entity* e);
 
 int main() {
     Player player;
     player.Move(10, 10);
     std::cout << player.X << " " << player.Y << std::endl;
     std::cout << sizeof(player) << std::endl;
+
+    Entity entity;
+    PrintName(&entity);
+    PrintName(&player);
     return 0;
+}
+
+
+void PrintName(Entity* e) {
+    std::cout << e->GetName() << std::endl;
 }
