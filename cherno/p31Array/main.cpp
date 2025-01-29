1da1bdf04360c41a71cc4312977c78f8d2310e5b
Ferris <ferris.evans@outlook.com>
2025-01-29 23:00:21 +0800

    p32 string

--- cherno/p31Array/main.cpp
+++ cherno/p31Array/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+void string();
+
 int main() {
 
     // allocate mem on stack. mem will release when scope is over
@@ -10,5 +12,13 @@ int main() {
     for (int i = 0; i < 5; i++) another[i] = i * 1024;
     delete[] another;
     std::cout << "Hello, World!" << std::endl;
+    string();
     return 0;
+}
+
+void string() {
+    const char* s = "Hello, World!";
+    std::cout << s << std::endl;
+    char c[6] = {'a', 'b', 'c', 'd', 'e', 0};
+    std::cout << c << std::endl;
 }
